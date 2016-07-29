dad39a0594e7c3faa078f7ee972094095cdcf461
Artem Sheremet <dot.doom@gmail.com>
2016-07-29 07:00:44 +0200

    Don't render garbage on web preview (details) page

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ module.exports = {
     blocks: {
         graphviz: {
             process: function(block) {
+                if (this.generator === "json") {
+                    // Rendering for gitbook.com, which won't allow HTML
+                    // for security reasons.
+                    return "Viz.js plugin (available in e-book mode)";
+                }
                 return Viz(block.body, {
                     engine: block.kwargs.engine || "dot",
                     format: "svg"
